eb4e93b6fdea7ef26732ca4d921f49c9d55c77f0
Okwoli-Godwin <okwolig60@gmail.com>
2023-08-17 14:38:51 +0100

    pushed

--- src/dist/Routes/userRoutes.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const controller_1 = require("../controller/controller");
-const UserRoute = (0, express_1.Router)();
-UserRoute.route("/createuser").post(controller_1.createUser);
-exports.default = UserRoute;
