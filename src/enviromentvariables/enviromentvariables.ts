1ce520544ca98370d571c819da0f425b751a55bb
Okwoli-Godwin <114069936+Okwoli-Godwin@users.noreply.github.com>
2023-08-18 08:08:18 +0100

    Delete src directory

--- src/enviromentvariables/enviromentvariables.ts
+++
@@ -1,8 +0,0 @@
-import dotenv from "dotenv";
-
-dotenv.config();
-
-export const environmentVariables = {
-    PORT: process.env.PORT as string,
-    MONGODBCONNECT: process.env.MONGODBCONNECT as string,
-};
